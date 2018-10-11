ffabfb6628c290c877b1a41488ca734a31a83eed
Jessica Berrett <jessicaberrett@Jessicas-MacBook-Pro.local>
2018-10-10 20:20:15 -0700

    component checkpoint

---
+++ src/components/ToDo.js
@@ -0,0 +1,12 @@
+ import React, { Component } from 'react';
+
+ class ToDo extends Component {
+   render() {
+     return (
+       <li> So much to do... </li>
+
+     );
+   }
+ }
+
+ export default ToDo;
